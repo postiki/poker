ebb48e8da179544d59d34052609b61ff53d4a18d
postiki <egorsheleznov@gmail.com>
2025-05-08 14:42:17 +0400

    feature: download and unpack sets

---
+++ download-and-unpack-small-set.py
@@ -0,0 +1,22 @@
+import kagglehub
+import os
+import shutil
+
+target_dir = "./data/data-set-classification"
+
+if not os.path.exists(target_dir):
+    os.makedirs(target_dir)
+
+path = kagglehub.dataset_download("gpiosenka/cards-image-datasetclassification")
+
+print("Path to dataset files:", path)
+
+if os.path.exists(path):
+    for item in os.listdir(path):
+        source = os.path.join(path, item)
+        destination = os.path.join(target_dir, item)
+        if os.path.isdir(source):
+            shutil.copytree(source, destination, dirs_exist_ok=True)
+        else:
+            shutil.copy2(source, destination)
+    print(f"Dataset has been unpacked to {target_dir}")
